ca901a40fe1ca93e67edadb68de5aa43183edf94
Denis Sipakov <denis.sipakov@pandadoc.com>
2018-11-19 23:19:22 +0300

    Add views

--- core/local_constants.py
+++
@@ -1,6 +0,0 @@
-import settings
-
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-SPREADSHEET_ID = '1tLXPFz6opyJQBFLafNXAXMULwtS8sjm94mpJVGO0R5I'
-CREDS_FILE = settings.PROJECT_ROOT + '/credentials.json'
-TOKEN_FILE = settings.PROJECT_ROOT + '/token.json'
